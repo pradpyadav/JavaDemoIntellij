38ed49a8562d5f9da6aba90e94d5c3e62a0ab692
Pradeep Yadav <pradpyadav@gmail.com>
2021-01-06 14:33:58 -0500

    Commit Model 1

---
+++ src/package1/Model1.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Model1 {
+    private String name;
+    private String modelId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
+    }
+}
