d373c277c4039aa80d0b0d5003adb06b7b0c67de
Pradeep Yadav <pradpyadav@gmail.com>
2021-01-06 14:38:54 -0500

    Commit Model 2

---
+++ src/package1/Model2.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Model2 {
+    private String modelName;
+    private int modelNumber;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+}
